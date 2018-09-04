7c45ccb01e3a615fc57c5fd0c65810a9835ce9a1
victor de lange <victordelange2000@hotmail.com>
2018-09-04 11:17:17 +0200

    Prompt en document write

---
+++ lab1.js
@@ -0,0 +1,5 @@
+ alert("Hello i am logging you in");
+ document.write(prompt("vul je voornaam in", ""));
+ document.write(prompt("vul je achternaam in"));
+ document.write("<br />");
+ document.write(prompt("vul je leeftijd in"));
